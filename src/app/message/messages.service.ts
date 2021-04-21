e0744b3869434f8dcd668eec5719dd72a28a65fa
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2021-04-21 14:52:03 +0200

    Add messages stream

--- src/app/message/messages.service.ts
+++ src/app/message/messages.service.ts
@@ -1,10 +1,39 @@
-import { Injectable } from '@angular/core';
+import { Message } from "../message/message.model";
+import { Injectable } from "@angular/core";
+import { Observable, Subject } from "rxjs";
+import { Thread } from "../thread/thread.model";
+import { User } from "../user/user.model";
+import { filter, scan } from "rxjs/operators";
 
+const initialMessages: Message[] = [];
+
+interface IMessagesOperation extends Function {
+  (messages: Message[]): Message[];
+}
 
 @Injectable()
 export class MessagesService {
+  newMessages: Subject<Message> = new Subject<Message>();
+  messages: Observable<Message[]>;
+  updates: Subject<any> = new Subject<any>();
 
   constructor() {
+    this.messages = this.updates.pipe(
+      scan((messages: Message[], operation: IMessagesOperation) => {
+        return operation(messages);
+      }, initialMessages)
+    );
   }
 
+  addMessage(message: Message): void {
+    this.newMessages.next(message);
+  }
+
+  messagesForThreadUser(thread: Thread, user: User): Observable<Message> {
+    return this.newMessages.pipe(
+      filter((message: Message) => {
+        return message.thread.id === thread.id && message.author.id !== user.id;
+      })
+    );
+  }
 }
